c758ef6f6df1c89edefec726d71328b30c216daa
AnthonyFugon <44510991+AnthonyFugon@users.noreply.github.com>
2018-11-23 18:12:45 -0600

    Login

---
+++ Inspinia_MVC5_SeedProject/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inspinia_MVC5_SeedProject.Models;
+
+namespace Inspinia_MVC5_SeedProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+                return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(tbTiposPago Login)
+        {
+            string user = Login.tpago_Codigo.ToString();
+            if (user == "admin" && Login.tpago_Descripcion == "Admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
