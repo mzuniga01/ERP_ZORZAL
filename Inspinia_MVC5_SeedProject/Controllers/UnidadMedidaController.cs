5d998e55802f2f3e0c4d3a1b515bcda44d354e66
MaFerSosa19 <44510955+MaFerSosa19@users.noreply.github.com>
2018-11-30 15:12:01 -0600

    G6 - Unidad de Medida
    Catalogo de Unidades de Medida

---
+++ Inspinia_MVC5_SeedProject/Controllers/UnidadMedidaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP_ZORZAL.Models;
+
+namespace ERP_ZORZAL.Controllers
+{
+    public class UnidadMedidaController : Controller
+    {
+        private ERP_ZORZALEntities db = new ERP_ZORZALEntities();
+
+        // GET: /UnidadMedida/
+        public ActionResult Index()
+        {
+            return View(db.tbUnidadMedida.ToList());
+        }
+
+        // GET: /UnidadMedida/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbUnidadMedida tbUnidadMedida = db.tbUnidadMedida.Find(id);
+            if (tbUnidadMedida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbUnidadMedida);
+        }
+
+        // GET: /UnidadMedida/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /UnidadMedida/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="uni_Id,uni_Descripcion,uni_Abreviacion,uni_UsuarioCrea,uni_FechaCrea,uni_UsuarioModifica,uni_FechaModifica")] tbUnidadMedida tbUnidadMedida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbUnidadMedida.Add(tbUnidadMedida);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbUnidadMedida);
+        }
+
+        // GET: /UnidadMedida/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbUnidadMedida tbUnidadMedida = db.tbUnidadMedida.Find(id);
+            if (tbUnidadMedida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbUnidadMedida);
+        }
+
+        // POST: /UnidadMedida/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="uni_Id,uni_Descripcion,uni_Abreviacion,uni_UsuarioCrea,uni_FechaCrea,uni_UsuarioModifica,uni_FechaModifica")] tbUnidadMedida tbUnidadMedida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbUnidadMedida).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbUnidadMedida);
+        }
+
+        // GET: /UnidadMedida/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbUnidadMedida tbUnidadMedida = db.tbUnidadMedida.Find(id);
+            if (tbUnidadMedida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbUnidadMedida);
+        }
+
+        // POST: /UnidadMedida/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbUnidadMedida tbUnidadMedida = db.tbUnidadMedida.Find(id);
+            db.tbUnidadMedida.Remove(tbUnidadMedida);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
