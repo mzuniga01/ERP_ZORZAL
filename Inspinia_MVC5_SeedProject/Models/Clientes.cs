0f58d048eefb2fd806a5722b39c4c73969cee90a
Maria Alejandra Funez <44504798+Alejandrafp@users.noreply.github.com>
2018-12-02 14:04:24 -0600

    Prototipo grupo 1
    Se modifico Cliente, Factura, Cuenta de banco, Punto de emision

---
+++ Inspinia_MVC5_SeedProject/Models/Clientes.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_ZORZAL.Models
+{
+    [MetadataType(typeof(ClientesMetaData))]
+
+    public partial class tbCliente
+    {
+
+    }
+    public class ClientesMetaData
+    {
+        [Display(Name = "Id Cliente")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public int clte_Id { get; set; }
+        [Display(Name = "Número de Identificación")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_RTN_Identidad_Pasaporte { get; set; }
+        [Display(Name = "¿Es Persona Natural?")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public bool clte_EsPersonaNatural { get; set; }
+        [Display(Name = "Nombre")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Nombres { get; set; }
+        [Display(Name = "Apellido")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Apellidos { get; set; }
+        [Display(Name = "Fecha Nacimiento")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public System.DateTime clte_FechaNacimiento { get; set; }
+        [Display(Name = "Nacionalidad")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Nacionalidad { get; set; }
+        [Display(Name = "Sexo")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Sexo { get; set; }
+        [Display(Name = "Teléfono")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Telefono { get; set; }
+        [Display(Name = "Nombre Comercial")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_NombreComercial { get; set; }
+        [Display(Name = "Razón Social")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_RazonSocial { get; set; }
+        [Display(Name = "Contacto Nombre")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_ContactoNombre { get; set; }
+        [Display(Name = "Contacto Email")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_ContactoEmail { get; set; }
+        [Display(Name = "Contacto Teléfono")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_ContactoTelefono { get; set; }
+        [Display(Name = "Fecha Constitución")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public Nullable<System.DateTime> clte_FechaConstitucion { get; set; }
+        public string mun_Codigo { get; set; }
+        [Display(Name = "Dirección")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Direccion { get; set; }
+        [Display(Name = "Correo Electrónico")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_CorreoElectronico { get; set; }
+        [Display(Name = "Es Activo?")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_EsActivo { get; set; }
+        [Display(Name = "Razón Inactivo")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_RazonInactivo { get; set; }
+        [Display(Name = "Con Crédito?")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public bool clte_ConCredito { get; set; }
+        [Display(Name = "Es Minorista?")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public bool clte_EsMinorista { get; set; }
+        [Display(Name = "Observaciones")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string clte_Observaciones_ { get; set; }
+        [Display(Name = "Usuario Crea")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public int clte_UsuarioCrea { get; set; }
+        [Display(Name = "Fecha Crea")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public System.DateTime clte_FechaCrea { get; set; }
+        [Display(Name = "Usuario Modificó")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public Nullable<int> clte_UsuarioModifica { get; set; }
+        [Display(Name = "CFecha Modificó")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public Nullable<System.DateTime> clte_FechaModifica { get; set; }
+    }
+}
--- Inspinia_MVC5_SeedProject/Scripts/app/PersonaNatural.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/PersonaNatural.js
@@ -1,50 +1,26 @@
-﻿(document).ready(function () {
-
-    $('#subscribe').on('click', function () {
-        var c = document.getElementById('subscribe').checked;
-        if (c) {
-
-            $("#apellido").hide();
-
-        }
-
-        else {
-            $("#apellido").show();
-
-        }
-
-
-    });
-
+﻿$("#clte_EsPersonaNatural").change(function () {
+    if (this.checked) {
+        //Do stuff
+        console.log("Hola");
+        $('#natural').show();
+        $('#juridica').hide();
+    }
+    else {
+        $('#natural').hide();
+        $('#juridica').show();
+    }
 });
 
 
-
-//$(document).ready(function () {
-//    $('#suscribe').on('change', function () {
-//        if (this.checked) {
-//            $("#nombrecomercial").hide();
-//            $("#razonsocial").hide();
-//            $("#contactonombre").hide();
-//            $("#contactoemail").hide();
-//            $("#contactotel").hide();
-//            $("#fechaconstitucion").hide();
-//        }
-//        else {
-//            $("#nombre").show();
-//            $("#apellido").show();
-//            $("#fechanacimiento").show();
-//            $("#nacionalidad").show();
-//            $("#sexo").show();
-//            $("#telefono").show();
-//            $("#municipio").show();
-//            $("#direccion").show();
-//            $("#correoelectronico").show();
-//            $("#activo").show();
-//            $("#clientecredito").show();
-//            $("#minorista").show();
-//            $("#observaciones").show();
-
-//        }
-//    })
-//});
+$("#clte_EsPersonaNatural").ready(function () {
+    if (this.checked) {
+        //Do stuff
+        console.log("Hola");
+        $('#natural').show();
+        $('#juridica').hide();
+    }
+    else {
+        $('#natural').hide();
+        $('#juridica').show();
+    }
+});
