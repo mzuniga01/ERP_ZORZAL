b82f955262be13cc85e33f835492b171a78b0f89
MaFerSosa19 <44510955+MaFerSosa19@users.noreply.github.com>
2018-12-02 18:11:48 -0600

    G6 - Prototipos
    Tablas Box(Cajas), Producto, RolesUsuario,Salida y SalidaDetalles, TiposSalida, UnidadMedida y Usuario

---
+++ Inspinia_MVC5_SeedProject/Controllers/SalidaController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP_ZORZAL.Models;
+
+namespace ERP_ZORZAL.Controllers
+{
+    public class SalidaController : Controller
+    {
+        private ERP_ZORZALEntities db = new ERP_ZORZALEntities();
+
+        // GET: /Salida/
+        public ActionResult Index()
+        {
+            var tbsalida = db.tbSalida.Include(t => t.tbBodega).Include(t => t.tbBox).Include(t => t.tbEstadoMovimiento).Include(t => t.tbFactura).Include(t => t.tbTipoSalida);
+            return View(tbsalida.ToList());
+        }
+
+        public ActionResult _IndexSalidaDetalle()
+        {
+            return View();
+        }
+
+        public ActionResult _DetailsSalidaDetalle()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        // GET: /Salida/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    tbSalida tbSalida = db.tbSalida.Find(id);
+        //    if (tbSalida == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(tbSalida);
+        //}
+
+        // GET: /Salida/Create
+        public ActionResult Create()
+        {
+            ViewBag.bod_Id = new SelectList(db.tbBodega, "bod_Id", "bod_ResponsableBodega");
+            ViewBag.box_Codigo = new SelectList(db.tbBox, "box_Codigo", "box_Descripcion");
+            ViewBag.estm_Id = new SelectList(db.tbEstadoMovimiento, "estm_Id", "estm_Descripcion");
+            ViewBag.fact_Id = new SelectList(db.tbFactura, "fact_Id", "fact_Codigo");
+            ViewBag.tsal_Id = new SelectList(db.tbTipoSalida, "tsal_Id", "tsal_Descripcion");
+            return View();
+        }
+
+        // POST: /Salida/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="sal_Id,bod_Id,fact_Id,sal_FechaElaboracion,estm_Id,box_Codigo,tsal_Id,sal_RazonDevolucion,sal_UsuarioCrea,sal_FechaCrea,sal_UsuarioModifica,sal_FechaModifica")] tbSalida tbSalida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbSalida.Add(tbSalida);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.bod_Id = new SelectList(db.tbBodega, "bod_Id", "bod_ResponsableBodega", tbSalida.bod_Id);
+            ViewBag.box_Codigo = new SelectList(db.tbBox, "box_Codigo", "box_Descripcion", tbSalida.box_Codigo);
+            ViewBag.estm_Id = new SelectList(db.tbEstadoMovimiento, "estm_Id", "estm_Descripcion", tbSalida.estm_Id);
+            ViewBag.fact_Id = new SelectList(db.tbFactura, "fact_Id", "fact_Codigo", tbSalida.fact_Id);
+            ViewBag.tsal_Id = new SelectList(db.tbTipoSalida, "tsal_Id", "tsal_Descripcion", tbSalida.tsal_Id);
+            return View(tbSalida);
+        }
+
+        // GET: /Salida/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    tbSalida tbSalida = db.tbSalida.Find(id);
+        //    if (tbSalida == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.bod_Id = new SelectList(db.tbBodega, "bod_Id", "bod_ResponsableBodega", tbSalida.bod_Id);
+        //    ViewBag.box_Codigo = new SelectList(db.tbBox, "box_Codigo", "box_Descripcion", tbSalida.box_Codigo);
+        //    ViewBag.estm_Id = new SelectList(db.tbEstadoMovimiento, "estm_Id", "estm_Descripcion", tbSalida.estm_Id);
+        //    ViewBag.fact_Id = new SelectList(db.tbFactura, "fact_Id", "fact_Codigo", tbSalida.fact_Id);
+        //    ViewBag.tsal_Id = new SelectList(db.tbTipoSalida, "tsal_Id", "tsal_Descripcion", tbSalida.tsal_Id);
+        //    return View(tbSalida);
+        //}
+
+        
+
+
+        // POST: /Salida/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="sal_Id,bod_Id,fact_Id,sal_FechaElaboracion,estm_Id,box_Codigo,tsal_Id,sal_RazonDevolucion,sal_UsuarioCrea,sal_FechaCrea,sal_UsuarioModifica,sal_FechaModifica")] tbSalida tbSalida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbSalida).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.bod_Id = new SelectList(db.tbBodega, "bod_Id", "bod_ResponsableBodega", tbSalida.bod_Id);
+            ViewBag.box_Codigo = new SelectList(db.tbBox, "box_Codigo", "box_Descripcion", tbSalida.box_Codigo);
+            ViewBag.estm_Id = new SelectList(db.tbEstadoMovimiento, "estm_Id", "estm_Descripcion", tbSalida.estm_Id);
+            ViewBag.fact_Id = new SelectList(db.tbFactura, "fact_Id", "fact_Codigo", tbSalida.fact_Id);
+            ViewBag.tsal_Id = new SelectList(db.tbTipoSalida, "tsal_Id", "tsal_Descripcion", tbSalida.tsal_Id);
+            return View(tbSalida);
+        }
+
+        // GET: /Salida/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbSalida tbSalida = db.tbSalida.Find(id);
+            if (tbSalida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbSalida);
+        }
+
+        // POST: /Salida/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbSalida tbSalida = db.tbSalida.Find(id);
+            db.tbSalida.Remove(tbSalida);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Inspinia_MVC5_SeedProject/Controllers/TiposSalidaController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP_ZORZAL.Models;
+
+namespace ERP_ZORZAL.Controllers
+{
+    public class TiposSalidaController : Controller
+    {
+        private ERP_ZORZALEntities db = new ERP_ZORZALEntities();
+
+        // GET: /TiposSalida/
+        public ActionResult Index()
+        {
+            return View(db.tbTipoSalida.ToList());
+        }
+
+        // GET: /TiposSalida/Details/5
+        public ActionResult Details(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbTipoSalida tbTiposSalida = db.tbTipoSalida.Find(id);
+            if (tbTiposSalida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbTiposSalida);
+        }
+
+        public ActionResult Crear()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+
+            return View();
+        }
+
+        public ActionResult Detalles()
+        {
+
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+
+            return View();
+        }
+
+        public ActionResult Detalles2()
+        {
+
+            return View();
+        }
+        // GET: /TiposSalida/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /TiposSalida/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="tsal_Id,tsal_Descripcion,tsal_UsuarioCrea,tsal_FechaCrea,tsal_UsuarioModifica,tsal_FechaModifica")] tbTiposSalida tbTiposSalida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbTipoSalida.Add(tbTiposSalida);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbTiposSalida);
+        }
+
+        // GET: /TiposSalida/Edit/5
+        public ActionResult Edit(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbTipoSalida tbTiposSalida = db.tbTipoSalida.Find(id);
+            if (tbTiposSalida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbTiposSalida);
+        }
+
+        // POST: /TiposSalida/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="tsal_Id,tsal_Descripcion,tsal_UsuarioCrea,tsal_FechaCrea,tsal_UsuarioModifica,tsal_FechaModifica")] tbTiposSalida tbTiposSalida)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbTiposSalida).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbTiposSalida);
+        }
+
+        // GET: /TiposSalida/Delete/5
+        public ActionResult Delete(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbTipoSalida tbTiposSalida = db.tbTipoSalida.Find(id);
+            if (tbTiposSalida == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbTiposSalida);
+        }
+
+        // POST: /TiposSalida/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte id)
+        {
+            tbTipoSalida tbTiposSalida = db.tbTipoSalida.Find(id);
+            db.tbTipoSalida.Remove(tbTiposSalida);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
