dfe0b5f189ac416d6e89dc66c0234940521b439c
Alejandrafp <44504798+Alejandrafp@users.noreply.github.com>
2018-12-06 08:27:02 -0600

    Final catalogo Grupo 1

--- Inspinia_MVC5_SeedProject/Scripts/app/Banco.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/Banco.js
@@ -1,21 +1,16 @@
-﻿//$("#ban_Nombre").change(function () {
-//    var str = $("#ban_Nombre").val();
-//    var res = str.toUpperCase();
-//    $("#ban_Nombre").val(res);
+﻿$("#ban_Nombre").change(function () {
+    var str = $("#ban_Nombre").val();
+    var res = str.toUpperCase();
+    $("#ban_Nombre").val(res);
+});
 
-//    document.getElementById("ban_Nombre").value = document.getElementById("ban_Nombre").value.toUpperCase();
-
-
-//});
-
-
-$(document).ready(function () {
-    $(ban_Nombre).on("keypress", function () {
-        $input = $(this);
-        setTimeout(function () {
-            $input.val($input.val().toUpperCase());
-        }, 50);
-    })
+$(ban_Nombre).on("keypress", function () {
+    $input = $(this);
+    setTimeout(function () {
+        $input.val($input.val().toUpperCase());
+    }, 50);
 })
 
-
+//$(document).ready(function () {
+//    $('#ban_TelefonoContacto').mask('(00) 0000-0000');
+//});
