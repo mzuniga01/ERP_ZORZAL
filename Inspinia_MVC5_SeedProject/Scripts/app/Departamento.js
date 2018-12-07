be065225dc39186ccff2943e2a2ae2c5f138023a
JoshuaHerrera12345 <44504972+JoshuaHerrera12345@users.noreply.github.com>
2018-12-07 08:18:53 -0600

    SpGetMunicipios Vista Cliente

--- Inspinia_MVC5_SeedProject/Scripts/app/Departamento.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/Departamento.js
@@ -1,9 +1,9 @@
-﻿$(document).on("change", dep_Codigo, function () {
+﻿$(document).on("change", '#dep_Codigo', function () {
     GetMunicipios();
 });
  
 function GetMunicipios() {
-    var CodDepartamento = $(dep_Codigo).val();
+    var CodDepartamento = $('#dep_Codigo').val();
     $.ajax({
         url: "/Cliente/GetMunicipios",
         method: "POST",
@@ -13,16 +13,18 @@ function GetMunicipios() {
     })
     .done(function (data) {
         if (data.length > 0) {
-            $(mun_Codigo).empty();
-            $(mun_Codigo).append("<option value=''>Seleccione</option>");
+            $('#mun_Codigo').empty();
+            $('#mun_Codigo').append("<option value=''>Seleccione Municipio</option>");
             $.each(data, function (key, val) {
-                $(mun_Codigo).append("<option value=" + val.mun_Codigo + ">" + val.mun_Nombre + "</option>");
+                $('#mun_Codigo').append("<option value=" + val.mun_Codigo + ">" + data + "</option>");
             });
-            $(mun_Codigo).trigger("chosen:updated");
+ 
+            $('#mun_Codigo').trigger("chosen:updated");
         }
         else {
-            $(mun_Codigo).empty();
-            $(mun_Codigo).append("<option value=''>Seleccione</option>");
+            $('#mun_Codigo').empty();
+            $('#mun_Codigo').append("<option value=''>Seleccione Municipio</option>");
         }
     });
+
 }
