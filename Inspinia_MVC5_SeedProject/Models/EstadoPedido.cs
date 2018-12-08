b48a33460b85eaf2c7cd82058e341a2a7c5f072c
Maria Alejandra Funez <44504798+Alejandrafp@users.noreply.github.com>
2018-12-08 15:29:48 -0600

    Actualizacion
    Se realizo la actualizacion de la tabla Pedido

---
+++ Inspinia_MVC5_SeedProject/Models/EstadoPedido.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_ZORZAL.Models
+{
+    [MetadataType(typeof(EstadoPedidoMetaData))]
+    public partial class tbEstadoPedido
+    {
+
+    }
+
+
+    public class EstadoPedidoMetaData
+    {
+        [Display(Name = "Id")]
+        public byte esped_Id { get; set; }
+
+        [Display(Name = "Estado")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public string esped_Descripcion { get; set; }
+
+        [Display(Name = "Usuario Creación")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public int esped_UsuarioCrea { get; set; }
+
+
+        [Display(Name = "Fecha Creación")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El campo {0} es requerido")]
+        public System.DateTime esped_FechaCrea { get; set; }
+
+        [Display(Name = "Usuario Modificación")]
+        public Nullable<int> esped_UsuarioModifica { get; set; }
+
+        [Display(Name = "Fecha Modificación")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> esped_FechaModifica { get; set; }
+    }
+}
---
+++ Inspinia_MVC5_SeedProject/Models/Pedidos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_ZORZAL.Models
+{
+    [MetadataType(typeof(PedidosMetaData))]
+
+    public partial class tbPedido
+    {
+    }
+
+    public class PedidosMetaData
+    {
+        [Display(Name ="Número Pedido")]
+        [Required]
+        public int ped_Id { get; set; }
+
+        [Display(Name ="Estado")]
+        public byte esped_Id { get; set; }
+
+        [Display(Name = "Fecha Elaboración")]
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}", HtmlEncode = false)]
+        public System.DateTime ped_FechaElaboracion { get; set; }
+
+        [Display(Name = "Fecha Entrega")]
+        [Required]
+        public System.DateTime ped_FechaEntrega { get; set; }
+
+        [Display(Name = "Número Cliente")]
+        [Required]
+        public int clte_Id { get; set; }
+
+        [Display(Name = "Sucursal")]
+        [Required]
+        public short suc_Id { get; set; }
+
+        [Display(Name = "Número Factura")]
+        [Required]
+        public long fact_Id { get; set; }
+
+        [Display(Name = "Usuario Creación")]
+        [Required]
+        public int ped_UsuarioCrea { get; set; }
+
+        [Display(Name = "Fecha Creación")]
+        [Required]
+        public System.DateTime ped_FechaCrea { get; set; }
+
+        [Display(Name = "Usuario Modificación")]
+        [Required]
+        public Nullable<int> ped_UsuarioModifica { get; set; }
+
+        [Display(Name = "Fecha Modificación")]
+        [Required]
+        public Nullable<System.DateTime> ped_FechaModifica { get; set; }
+
+    }
+}
