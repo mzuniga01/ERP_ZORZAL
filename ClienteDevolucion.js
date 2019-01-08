6219e353126374ca879ad5525569a5a8ff31d433
gdavis10 <44504954+gdavis10@users.noreply.github.com>
2019-01-08 07:47:47 -0600

    G3- Devolucion
    Actualizacion de devolucion

---
+++ ClienteDevolucion.js
@@ -0,0 +1,9 @@
+﻿//Devolucion Seleccionar Cliente
+$(document).on("click", "#DataTable tbody tr td button#AgregarCliente", function () {
+    idItem = $(this).closest('tr').data('id');
+    descItem = $(this).closest('tr').data('desc');
+    $("#tbFactura_clte_Identificacion").val(idItem);
+    $("#tbFactura_clte_Nombres").val(descItem);
+    $('#ModalAgregarCliente').modal('hide');
+
+});
---
+++ ProductoDevolucion.js
@@ -0,0 +1,10 @@
+﻿//Devolucion Seleccionar Producto
+$(document).on("click", "#DataTable1 tbody tr td button#Agregar", function () {
+    idItem = $(this).closest('tr').data('id');
+    DescItem = $(this).closest('tr').data('desc');
+    DescValor = $(this).closest('tr').data('valor');
+    $("#prod_Codigo").val(idItem);
+    $("#tbProducto_prod_Descripcion").val(DescItem);
+    $("#PrecioUnitario").val(DescValor);
+    $('#ModalBuscarProducto').modal('hide');
+});
