ced8d2c96ec4cc04f57663572f3ecbdf8a82681b
luisb05 <44504830+luisb05@users.noreply.github.com>
2019-01-09 16:46:44 -0600

    Factura

--- Inspinia_MVC5_SeedProject/Scripts/app/ClienteFactura.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/ClienteFactura.js
@@ -22,12 +22,47 @@ $(document).ready(function () {
 $(document).on("click", "#tbCliente tbody tr td button#seleccionar", function () {
     idItem = $(this).closest('tr').data('id');
     rtnItem = $(this).closest('tr').data('rtn');
-    nombreItem = $(this).closest('tr').data('nombrecliente');sss
+    nombreItem = $(this).closest('tr').data('nombrecliente');
+    tpid = $(this).closest('tr').data('tpi');
+    Fecha = $(this).closest('tr').data('fecha');
+    EspersonaNatural = $(this).closest('tr').data('EsPersonaNatural');
+
+   var espersonal = $('.clte_EsPersonaNatural')[3].checked = true;
+   console.log()     
     $("#clte_Id").val(idItem);
     $("#clte_Identificacion").val(rtnItem);
     $("#clte_Nombres").val(nombreItem);
+    $("#tpi_Id").val(tpid);
+    $("#clte_Fecha").val(Fecha);
     $('#ModalAgregarCliente').modal('hide');
+    console.log(EspersonaNatural)
+    if (EspersonaNatural) {
+        $('#TerceraEdad').show();
+        //Tercera Edad
+        ms = Date.parse(Fecha);
+        fecha1 = new Date(ms);
+        console.log(fecha1)
+        var Fechas1 = fecha1.getFullYear()
+        var today = new Date
+        var today1 = today.getFullYear()
+        if (today1 - Fechas1 >= 60) {
 
+        }
+        else {
+            $("#MostrarTerceraEdad").prop("checked", true);
+            $("#fact_AutorizarDescuento").prop("checked", true);
+            $('#Cred2').show();
+            $('#fact_PorcentajeDescuento').val('');
+            document.getElementById("MostrarTerceraEdad").disabled = true;
+            document.getElementById("fact_AutorizarDescuento").disabled = true;
+        }
+    }
+    else {
+        $('#TerceraEdad').hide();
+        document.getElementById("MostrarTerceraEdad").disabled = false;
+        document.getElementById("fact_AutorizarDescuento").disabled = false;
+        $('#Cred2').hide();
+    }
 });
 
 
--- Inspinia_MVC5_SeedProject/Scripts/app/Factura.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/Factura.js
@@ -16,3 +16,51 @@
         }
     });
 });
+
+
+$("#consumidorFinal").change(function () {
+   if (this.checked) {
+        //Do stuff
+       $('#DatosCliente').hide();
+       $("#clte_Identificacion").val('****');
+       $("#clte_Nombres").val('****');
+       $("#clte_Id").val(1);
+        
+    }
+    else {
+       $('#DatosCliente').show();
+       $("#clte_Nombres").val('');
+       $("#clte_Identificacion").val('');
+    }
+})
+
+
+
+//$().change(function () {
+//    var Identificacion = $("#tpi_Id").val()
+//    console.log(Identificacion);
+//    valido = document.getElementById('label_identificacion');
+//    document.getElementById('label_identificacion').innerHTML = Identificacion;
+//})
+//$(document).ready(function () {
+//    var Identificacion = $("#tpi_Id").val()
+//    console.log(Identificacion);
+//    valido = document.getElementById('label_identificacion');
+//    document.getElementById('label_identificacion').innerHTML = Identificacion;;
+
+//});
+
+$("#consumidorFinal").ready(function () {
+    if (this.checked) {
+        //Do stuff
+        $('#DatosCliente').hide();
+        $("#clte_Identificacion").val('****');
+        $("#clte_Nombres").val('****');
+        $("#clte_Id").val(0);
+  }
+    else {
+        $('#DatosCliente').show();
+        $("#clte_Nombres").val('');
+        $("#clte_Identificacion").val('');
+    }
+});
