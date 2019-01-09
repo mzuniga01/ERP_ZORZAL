01868d59493703021fdd8f05a84e34cba631840f
gdavis10 <44504954+gdavis10@users.noreply.github.com>
2019-01-09 12:10:05 -0600

    G3- Devolucion y Movimiento Caja

--- Inspinia_MVC5_SeedProject/Scripts/app/ProductoDevolucion.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/ProductoDevolucion.js
@@ -8,3 +8,16 @@ $(document).on("click", "#DataTable1 tbody tr td button#Agregar", function () {
     $("#PrecioUnitario").val(DescValor);
     $('#ModalBuscarProducto').modal('hide');
 });
+
+
+//Devolucion Agregar Producto
+$(document).on("click", "#DataTable1 tbody tr td button#Producto", function () {
+    ProductoDescripcionItem = $(this).closest('tr').data('productodescripcion');
+    CodigoFacturaItem = $(this).closest('tr').data('codigofactura');
+    PorcentajeDescuentoItem = $(this).closest('tr').data('porcentajedescuento')
+    PrecioUnitarioItem = $(this).closest('tr').data('preciounitario')
+    $("#tbProducto_prod_Descripcion").val(ProductoDescripcionItem);
+    $("#tbDevolucion_tbFactura_fact_Codigo").val(CodigoFacturaItem);
+    $("#tbDevolucion_tbFactura_fact_PorcentajeDescuento").val(PorcentajeDescuentoItem);
+    $("#test_factd_PrecioUnitario").val(PrecioUnitarioItem);
+});
