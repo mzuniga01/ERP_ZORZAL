a871369bc06ac654ddb0e2127f588008d20a7c5f
kcire20 <44504854+kacire20@users.noreply.github.com>
2019-01-13 22:37:14 -0600

    Actualización G2

--- Inspinia_MVC5_SeedProject/Controllers/PedidoController.cs
+++ Inspinia_MVC5_SeedProject/Controllers/PedidoController.cs
@@ -42,7 +42,7 @@ namespace ERP_ZORZAL.Controllers
         }
 
 
-      public ActionResult _IndexCliente()
+        public ActionResult _IndexCliente()
         {
             return PartialView();
         }
@@ -71,7 +71,7 @@ namespace ERP_ZORZAL.Controllers
             tbPedido Pedido = new tbPedido();
             Pedido.suc_Id = 1;
             return View(Pedido);
-            
+
         }
 
         // POST: /Pedido/Create
@@ -261,32 +261,40 @@ namespace ERP_ZORZAL.Controllers
         // POST: /Pedido/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int? id,[Bind(Include= "ped_Id,esped_Id,ped_FechaElaboracion,ped_FechaEntrega,clte_Id,suc_Id,fact_Id,ped_UsuarioCrea,ped_FechaCrea")] tbPedido tbPedido)
+        public ActionResult Edit(int? id, [Bind(Include = "ped_Id,esped_Id,ped_FechaElaboracion,ped_FechaEntrega,clte_Id,suc_Id,fact_Id,ped_UsuarioCrea,ped_FechaCrea")] tbPedido tbPedido)
         {
             if (ModelState.IsValid)
             {
                 try
                 {
-                    tbPedido vPedido = db.tbPedido.Find(id);
-                    //db.tbTipoIdentificacion.Add(tbTipoIdentificacion);
-                    //db.SaveChanges();
-                    //return RedirectToAction("Index");
+                    ViewBag.ped_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioCrea);
+                    ViewBag.ped_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioModifica);
+                    ViewBag.clte_Id = new SelectList(db.tbCliente, "clte_Id", "clte_RTN_Identidad_Pasaporte", tbPedido.clte_Id);
+                    ViewBag.fact_Id = new SelectList(db.tbFactura, "fact_Id", "fact_Codigo", tbPedido.fact_Id);
+                    ViewBag.suc_Id = new SelectList(db.tbSucursal, "suc_Id", "mun_Codigo", tbPedido.suc_Id);
+                    ViewBag.esped_Id = new SelectList(db.tbEstadoPedido, "esped_Id", "esped_Descripcion", tbPedido.esped_Id);
+                    ViewBag.Producto = db.tbProducto.ToList();
+                    ViewBag.Cliente = db.tbCliente.ToList();
 
                     var MensajeError = "";
                     IEnumerable<object> list = null;
                     list = db.UDP_Vent_tbPedido_Update(tbPedido.ped_Id,
-                                                       tbPedido.esped_Id,
-                                                       tbPedido.ped_FechaElaboracion,
-                                                       tbPedido.ped_FechaEntrega,
-                                                       tbPedido.clte_Id,
-                                                       tbPedido.suc_Id,
-                                                       tbPedido.fact_Id,
-                                                       vPedido.ped_UsuarioCrea,
-                                                       vPedido.ped_FechaCrea);
-                    foreach (UDP_Vent_tbPedido_Update_Result Pedido in list)
-                        MensajeError = Pedido.MensajeError;
+                                                        tbPedido.esped_Id,
+                                                        tbPedido.ped_FechaElaboracion,
+                                                        tbPedido.ped_FechaEntrega,
+                                                        tbPedido.clte_Id,
+                                                        tbPedido.suc_Id,
+                                                        tbPedido.fact_Id,
+                                                        tbPedido.ped_UsuarioCrea,
+                                                        tbPedido.ped_FechaCrea);
+
+                    foreach (UDP_Vent_tbPedido_Update_Result ListaPrecio in list)
+                        MensajeError = ListaPrecio.MensajeError;
                     if (MensajeError == "-1")
                     {
 
@@ -295,14 +303,17 @@ namespace ERP_ZORZAL.Controllers
                     {
                         return RedirectToAction("Index");
                     }
-
                 }
                 catch (Exception Ex)
                 {
-                    var errors = ModelState.Values.SelectMany(v => v.Errors);
-                    Ex.Message.ToString();
+                    ModelState.AddModelError("", "No se pudo agregar el registros" + Ex.Message.ToString());
+                    ViewBag.listp_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioCrea);
+                    ViewBag.listp_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioModifica);
+                    ViewBag.listp_Id = new SelectList(db.tbListadoPrecioDetalle, "listp_Id", "prod_Codigo", tbPedido.ped_Id);
+                    ViewBag.Producto = db.tbProducto.ToList();
                 }
 
+                return RedirectToAction("Index");
             }
             ViewBag.ped_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioCrea);
             ViewBag.ped_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioModifica);
@@ -312,7 +323,62 @@ namespace ERP_ZORZAL.Controllers
             ViewBag.esped_Id = new SelectList(db.tbEstadoPedido, "esped_Id", "esped_Descripcion", tbPedido.esped_Id);
             ViewBag.Producto = db.tbProducto.ToList();
             return View(tbPedido);
+          
         }
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int? id,[Bind(Include= "ped_Id,esped_Id,ped_FechaElaboracion,ped_FechaEntrega,clte_Id,suc_Id,fact_Id,ped_UsuarioCrea,ped_FechaCrea")] tbPedido tbPedido)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            tbPedido vPedido = db.tbPedido.Find(id);
+        //            //db.tbTipoIdentificacion.Add(tbTipoIdentificacion);
+        //            //db.SaveChanges();
+        //            //return RedirectToAction("Index");
+
+        //            var MensajeError = "";
+        //            IEnumerable<object> list = null;
+        //            list = db.UDP_Vent_tbPedido_Update(tbPedido.ped_Id,
+        //                                               tbPedido.esped_Id,
+        //                                               tbPedido.ped_FechaElaboracion,
+        //                                               tbPedido.ped_FechaEntrega,
+        //                                               tbPedido.clte_Id,
+        //                                               tbPedido.suc_Id,
+        //                                               tbPedido.fact_Id,
+        //                                               vPedido.ped_UsuarioCrea,
+        //                                               vPedido.ped_FechaCrea);
+        //            foreach (UDP_Vent_tbPedido_Update_Result Pedido in list)
+        //                MensajeError = Pedido.MensajeError;
+        //            if (MensajeError == "-1")
+        //            {
+
+        //            }
+        //            else
+        //            {
+        //                return RedirectToAction("Index");
+        //            }
+
+        //        }
+        //        catch (Exception Ex)
+        //        {
+        //            var errors = ModelState.Values.SelectMany(v => v.Errors);
+        //            Ex.Message.ToString();
+        //        }
+
+        //    }
+        //    ViewBag.ped_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioCrea);
+        //    ViewBag.ped_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbPedido.ped_UsuarioModifica);
+        //    ViewBag.clte_Id = new SelectList(db.tbCliente, "clte_Id", "clte_RTN_Identidad_Pasaporte", tbPedido.clte_Id);
+        //    ViewBag.fact_Id = new SelectList(db.tbFactura, "fact_Id", "fact_Codigo", tbPedido.fact_Id);
+        //    ViewBag.suc_Id = new SelectList(db.tbSucursal, "suc_Id", "mun_Codigo", tbPedido.suc_Id);
+        //    ViewBag.esped_Id = new SelectList(db.tbEstadoPedido, "esped_Id", "esped_Descripcion", tbPedido.esped_Id);
+        //    ViewBag.Producto = db.tbProducto.ToList();
+        //    return View(tbPedido);
+        //}
 
         // GET: /Pedido/Delete/5
         public ActionResult Delete(int? id)
