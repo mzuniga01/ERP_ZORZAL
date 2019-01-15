b9c762fb1a5515af3f94351c7941a501f11a8edd
Alejandrafp <44504798+Alejandrafp@users.noreply.github.com>
2019-01-15 08:24:34 -0600

    Actualizacion
    Apertura Caja

--- Inspinia_MVC5_SeedProject/Scripts/app/Sucursal.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/Sucursal.js
@@ -99,3 +99,23 @@ $("#dep_Codigo").change(function () {
         document.getElementById("mun_Codigo").title = "";
     }
 });
+
+
+$('#suc_Correo').blur(function () {
+    campo = event.target;
+    valido = document.getElementById('emailOK');
+
+    var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    var regOficial = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    if (reg.test(campo.value) && regOficial.test(campo.value)) {
+        valido.innerText = "";
+    } else if (reg.test(campo.value)) {
+        valido.innerText = "";
+
+    } else {
+        valido.innerText = "Direccion de Correo Electronico Incorrecta";
+
+    }
+});
