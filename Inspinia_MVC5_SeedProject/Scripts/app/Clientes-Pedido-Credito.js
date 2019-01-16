d6c04e08a40f431d479b92f47e532a8587950d29
kcire20 <44504854+kacire20@users.noreply.github.com>
2019-01-16 01:42:45 -0600

    Actualización G2

--- Inspinia_MVC5_SeedProject/Scripts/app/Clientes-Pedido-Credito.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/Clientes-Pedido-Credito.js
@@ -1,14 +1,50 @@
-﻿
-//ERICK,Wilson JS
+﻿//ERICK,Wilson JS
 $(document).on("click", "#tbCliente tbody tr td button#seleccionar", function () {
     idItem = $(this).closest('tr').data('id');
     rtnItem = $(this).closest('tr').data('rtn');
     NombreCliente = $(this).closest('tr').data('name');
     $("#clte_Id").val(idItem);
     $("#tbCliente_clte_Identificacion").val(rtnItem);
-    $("#tbCliente_clte_Nombres").val(NombreCliente);
     $('#ModalAgregarClientes').modal('hide');
+    //var Comercial = $("#tbCliente_clte_NombreComercial").val()
+    $("#tbCliente_clte_Nombres").val(NombreCliente)
+    $("#tbCliente_clte_NombreComercial").val(NombreCliente);
+    //var Normal = $("#tbCliente_clte_Nombres").val()
+
+
+
+    //if (Normal != "**") {
+    //    $("#tbCliente_clte_Nombres").val(NombreCliente);
+
+    //    console.log("Si")
+    //    console.log(NombreCliente)
+    //}
+
+    //console.log(Normal)
+
+    //if (Normal == "**")
+    //{
+    //    $("#tbCliente_clte_NombreComercial").val(NombreCliente);
+    //    console.log(NombreCliente)
+      
+
+    //}
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).on("click", "#tbClienteSC tbody tr td button#seleccionar", function () {
     idItem = $(this).closest('tr').data('id');
@@ -19,4 +55,6 @@ $(document).on("click", "#tbClienteSC tbody tr td button#seleccionar", function 
     $("#tbCliente_clte_Nombres").val(nombreItem);
     $('#ModalAgregarClientes').modal('hide');
     //CargarAsignaciones();
+
+
 });
