64e2aebf818e697334ad770c11e0491564bb630c
kjargueta <44504821+kjargueta@users.noreply.github.com>
2019-01-18 23:56:27 -0600

    G3

--- Inspinia_MVC5_SeedProject/Scripts/app/ClienteExoneracion.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/ClienteExoneracion.js
@@ -1,23 +1,23 @@
-﻿//Exoneracion RowSeleccionar Cliente
+﻿$(function () {
+var from = $("#exo_FechaInicialVigencia").val();
+var to = $("#exo_FechaIFinalVigencia").val();
+
+if (Date.parse(from) > Date.parse(to)) {
+    valido = document.getElementById('cc');
+    valido.innerText = "Fecha inicial mayor a la fecha vencimiento";
+}
+});
+
+
+
+
 $(document).on("click", "#tbCliente tbody tr td button#seleccionar", function () {
     idItem = $(this).closest('tr').data('id');
     rtnItem = $(this).closest('tr').data('rtn');
     NombreCliente = $(this).closest('tr').data('name');
     $("#clte_Id").val(idItem);
     $("#tbCliente_clte_Identificacion").val(rtnItem);
-    $("#tbCliente_clte_Nombres").val(NombreCliente);
     $('#ModalAgregarClientes').modal('hide');
+    $("#tbCliente_clte_Nombres").val(NombreCliente)
+    $("#tbCliente_clte_NombreComercial").val(NombreCliente);
 });
-
-$(document).on("click", "#tbClienteSC tbody tr td button#seleccionar", function () {
-    idItem = $(this).closest('tr').data('id');
-    rtnItem = $(this).closest('tr').data('rtn');
-    nombreItem = $(this).closest('tr').data('nombre');
-    $("#clte_Id").val(idItem);
-    $("#tbCliente_clte_Identificacion").val(rtnItem);
-    $("#tbCliente_clte_Nombres").val(nombreItem);
-    $('#ModalAgregarClientes').modal('hide');
-});
-
-
-
