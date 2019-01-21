1d614d88609af49a8cfd7ab7e8050591198c5d46
luisb05 <44504830+luisb05@users.noreply.github.com>
2019-01-20 21:14:33 -0600

    Grupo #1

---
+++ Inspinia_MVC5_SeedProject/Scripts/app/GetCodigoNotaCredito.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    GetNumeroFact(1, 4)
+    $("#suc_Id").val(1);
+    $("#cja_Id").val(4);
+})
+function GetNumeroFact(CodSucursal, CodCaja) {
+    //var CodSucursal = $('#suc_Id').val();
+    //var CodCaja = $('#cja_Id').val();   
+
+    $.ajax({
+        url: "/NotaCredito/GetCodigoNotaCredito",
+        method: "POST",
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ CodSucursal: CodSucursal, CodCaja: CodCaja }),
+    })
+    .done(function (data) {
+        if (data.length > 0) {
+            var Mensaje = data;
+            if (Mensaje == -1) {
+                alert("El Numero CAI no tiene numeracion")
+                var url = $("#RedirectTo").val();
+                location.href = url;
+            }
+            else {
+                $('#nocre_Codigo').val(data);
+            }
+            //$.each(data, function (key, val) {
+
+            //});
+            //console.log(data)
+        }
+        else {
+
+        }
+    });
+}
