ea9a456c0f1d3877e7b92e39451213fe5f69b0fc
Jddubon14 <44510916+Jddubon14@users.noreply.github.com>
2019-02-08 15:09:21 -0600

    G6 Cambios

--- Inspinia_MVC5_SeedProject/Scripts/app/DetalleBox.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/DetalleBox.js
@@ -304,4 +304,14 @@ $('#btnCreateSalidaDetalle').click(function () {
     }
 });
 
+//VALIDAR SOLO NUMEROS
+$(function () {
+    $("#sald_Cantidad_SD").keydown(function (event) {
+        //alert(event.keyCode);
+        if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !== 190 && event.keyCode !== 110 && event.keyCode !== 8 && event.keyCode !== 9) {
+            return false;
+        }
+    });
+});
+
 
