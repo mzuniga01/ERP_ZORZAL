9f400033fbc26cc39b5d877efebb1b1bad729272
JoshuaHerrera12345 <44504972+JoshuaHerrera12345@users.noreply.github.com>
2019-02-17 19:19:47 -0600

    NotaCredito Error Eliminado

---
+++ Inspinia_MVC5_SeedProject/Scripts/app/CuponDescuentoEsImpreso.js
@@ -0,0 +1,23 @@
+﻿
+    function CuponEsImpreso() {
+        var cdtoId = $('#cdto_ID').val();
+        var EsImpreso = 1
+    $.ajax({
+        url: "/CuponDescuento/CuponEsImpreso",
+        method: "POST",
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ cdtoId: cdtoId, EsImpreso: EsImpreso }),
+    })
+
+    .done(function (data) {
+        if (data.length > 0) {
+            var url = $("#RedirectTo").val();
+            console.log(url)
+            location.href = url;
+        }
+        else {
+            alert("No Impreso");
+        }
+    });
+}
