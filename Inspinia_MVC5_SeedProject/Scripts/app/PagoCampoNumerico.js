5585902e4275c37100653c0a4a660fabfedd8778
spascon <celdon.pascon@bi-dss.com>
2019-02-18 10:45:24 -0600

    insidencias
    Pagos Tipo pago

---
+++ Inspinia_MVC5_SeedProject/Scripts/app/PagoCampoNumerico.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() {
+    //QUE SOLO ACEPTE
+    $("#pago_TotalPago")[0].maxLength = 12;
+    //VALIDAR SOLO NUMEROS
+
+    $(function () {
+        $("#pago_TotalPago").keydown(function (event) {
+            //alert(event.keyCode);
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !== 190 && event.keyCode !== 110 && event.keyCode !== 8 && event.keyCode !== 9) {
+                return false;
+            }
+        });
+
+    });
+
+   
+    $('#pago_TotalPago').on('input', function (e) {
+        if (!/^[0-9]+$/.test(this.value)) {
+            this.value = this.value.replace(/[^/^[0-9]+$/, "");
+        }
+    });
+
+});
