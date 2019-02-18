70d03eebacf82cb4169a7439adfc5c904d54e172
anjimenez <44504837+anjimenez@users.noreply.github.com>
2019-02-17 23:11:32 -0600

    SP Cliente

--- Inspinia_MVC5_SeedProject/Models/UDP_Vent_tbCliente_Estado_Result.cs
+++ Inspinia_MVC5_SeedProject/Models/UDP_Vent_tbCliente_Estado_Result.cs
@@ -11,7 +11,7 @@ namespace ERP_GMEDINA.Models
 {
     using System;
     
-    public partial class UDP_Vent_tbEstadoSolicitudCredito_Update_Result
+    public partial class UDP_Vent_tbCliente_Estado_Result
     {
         public string MensajeError { get; set; }
     }
