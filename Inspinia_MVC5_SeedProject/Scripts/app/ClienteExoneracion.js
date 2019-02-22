528f475c7b911e359ad946e2a97f02f4274771dd
anjimenez <44504837+anjimenez@users.noreply.github.com>
2019-02-22 16:16:05 -0600

    Incidencias Estado Factura

--- Inspinia_MVC5_SeedProject/Scripts/app/ClienteExoneracion.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/ClienteExoneracion.js
@@ -63,6 +63,12 @@ $('#exo_Documento').on('input', function (e) {
 });
 
 
+$('#suc_Direccion').on('input', function (e) {
+    if (!/^[ a-z0-9áéíóúüñ]*$/i.test(this.value)) {
+        this.value = this.value.replace(/[^ a-z0-9áéíóúüñ]+/ig, "");
+    }
+});
+
 //Documento fiscal
 
 
