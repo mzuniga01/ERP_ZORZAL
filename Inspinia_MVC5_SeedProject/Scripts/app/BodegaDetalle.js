6a4e840dcaa6fe0f12d7f9846758c4b55419fc05
gmacias20 <44510896+gmacias20@users.noreply.github.com>
2019-02-25 13:17:57 -0600

    G5-Bodega
    Tablas

--- Inspinia_MVC5_SeedProject/Scripts/app/BodegaDetalle.js
+++ Inspinia_MVC5_SeedProject/Scripts/app/BodegaDetalle.js
@@ -75,10 +75,13 @@ $(document).ready(function () {
                 "sNext": "Siguiente",
                 "sPrevious": "Anterior",
             },
+            "sZeroRecords": "No se encontraron resultados",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sEmptyTable": "No hay registros",
+            "sInfoEmpty": "Mostrando 0 de 0 Entradas",
             "sSearch": "Buscar",
             "sLengthMenu": "Mostrar _MENU_Registros Por Página",
-            "sInfo": "Mostrando _START_ a _END_ Entradas",
-             "sZeroRecords": "No se encontraron resultados",
+            "sInfo": "Mostrando _START_ a _END_ Entradas"
 
         },
 
@@ -96,9 +99,12 @@ $(document).ready(function () {
                 "sNext": "Siguiente",
                 "sPrevious": "Anterior",
             },
-          
-             "sSearch": "Buscar",
-            "sLengthMenu": "Mostrar _MENU_Registros ",
+            "sZeroRecords": "No se encontraron resultados",
+            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sEmptyTable": "No hay registros",
+            "sInfoEmpty": "Mostrando 0 de 0 Entradas",
+            "sSearch": "Buscar",
+            "sLengthMenu": "Mostrar _MENU_Registros Por Página",
             "sInfo": "Mostrando _START_ a _END_ Entradas"
         },
 
