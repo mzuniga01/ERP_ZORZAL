4c814942b7886457350b67242790abdb6563c8df
Mágdaly Zúniga <39597928+mzuniga01@users.noreply.github.com>
2020-01-25 21:42:40 -0600

    Menu

--- Inspinia_MVC5_SeedProject/Controllers/HomeController.cs
+++ Inspinia_MVC5_SeedProject/Controllers/HomeController.cs
@@ -13,17 +13,16 @@ namespace ERP_GMEDINA.Controllers
         [SessionManager("Home/Index")]
         public ActionResult Index()
         {
-            ViewData["SubTitle"] = "Welcome in ASP.NET MVC 5 INSPINIA SeedProject ";
-            ViewData["Message"] = "It is an application skeleton for a typical MVC 5 project. You can use it to quickly bootstrap your webapp projects.";
-
             return View();
         }
     
         public ActionResult Minor()
         {
-            ViewData["SubTitle"] = "Simple example of second view";
-            ViewData["Message"] = "Data are passing to view by ViewData from controller";
+            return View();
+        }
 
+        public ActionResult Main()
+        {
             return View();
         }
     }
